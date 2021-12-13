647cc00fc53b7c3b13831c061fb45442d95f9772
Fabo <fabo.romero97@gmail.com>
2021-12-13 13:47:40 -0300

    footer component is added and stylized

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react'
-
+import styles from './Footer.module.scss'
 const Footer: FC = () => {
     return (
-        <footer>
-            footer
+        <footer className={styles.footer}>
+            <p className={styles.copyright}>©2021</p>
+
+            <div className={styles.socialIcons}>
+                 <img src={'../../../../public/assets/socialIcons/facebook.svg'} alt="facebook"/>
+                 <img src={'../../../../public/assets/socialIcons/instagram.svg'} alt="instagram"/>
+                 <img src={'../../../../public/assets/socialIcons/linkedin.svg'} alt="linkedin"/>
+                 <img src={'../../../../public/assets/socialIcons/twitter.svg'} alt="twitter"/>
+                 <img src={'../../../../public/assets/socialIcons/youtube.svg'} alt="youtube"/>
+                 <img src={'../../../../public/assets/socialIcons/snapchat.svg'} alt="snapchat"/>
+            </div>
         </footer>
     )
 }
